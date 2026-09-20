148fdae3cef5b44188a6cfb99f2b48faf278c98a
Henry Strickland <strick@yak.net>
2026-09-20 08:29:51 -0400

    add 2 tasks to run?.sh

--- run9.sh
+++ run9.sh
@@ -1,1 +1,1 @@
-build/gep9 -disk0=build/disk0.dsk  build/kernel_6809.decb
+build/gep9 -disk0=build/disk0.dsk  -task1=build/rbf_6809.decb -task2=build/procfs_6809.decb "$@" build/kernel_6809.decb
--- runk.sh
+++ runk.sh
@@ -1,1 +1,1 @@
-build/gepk -disk0=build/disk0.dsk  build/kernel_68k.srec
+build/gepk -disk0=build/disk0.dsk  -task1=build/rbf_68k.srec -task2=build/procfs_68k.srec "$@" build/kernel_68k.srec
